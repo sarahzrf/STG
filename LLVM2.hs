1500922e616201e45ee9fafa7b9cdbe760282f3d
benzrf <benzrf@benzrf.com>
2017-12-21 22:49:33 -0500

    Shuffled a bit, now the musttails actually meet the requirements

--- LLVM2.hs
+++ LLVM2.hs
@@ -12,6 +12,7 @@ import Lam
 import LLVM.AST hiding (function, Name, Add, Sub)
 import qualified LLVM.AST as AST
 import qualified LLVM.AST.CallingConvention as CC
+import qualified LLVM.AST.Constant as K
 import qualified LLVM.AST.IntegerPredicate as IP
 import LLVM.AST.Type (i32)
 import LLVM.IRBuilder
@@ -19,105 +20,97 @@ import qualified LLVM.Module as L
 import LLVM.Context
 import Preamble
 
-uniq :: (Traversable t, Eq a) => t a -> ([b] -> t b, [a])
-uniq s =
-  runState (getCompose (traverse (Compose . state . replace) s)) []
-  where replace v seen = case findIndex (==v) seen of
-          Nothing -> ((!! length seen), seen ++ [v])
-          Just i  -> ((!! i), seen)
-
 -- looks like the place we'll need fresh names is in the monad that
 -- DOESN'T supply them...
-type ModB = StateT Int ModuleBuilder
-type FunB = IRBuilderT (ReaderT Env ModB)
+type ModB = ReaderT Env (StateT Int ModuleBuilder)
+type FunB = IRBuilderT ModB
 
-func ::
-  String -> Int -> ([Operand] -> FunB (Lam Operand)) -> FunB Operand
-func prefix pcount h = do
+data Pos = Res | Scrut deriving (Show)
+
+func :: Lam Operand -> ModB (Operand, [Operand])
+func l = do
+  let (li, free) = runState (traverse (state . replace) l) []
+      replace v seen = case findIndex (==v) seen of
+        Nothing -> (length seen, seen ++ [v])
+        Just i  -> (i, seen)
+
   cpt <- view closPtrType
   freshNum <- id <<+= 1
-  let funName = mkName (prefix ++ "." ++ show freshNum)
-      params = replicate pcount (cpt, NoParameterName)
-  lift $ repair <$> function funName params i32 (compile <=< h)
+  let funName = mkName ("thunk." ++ show freshNum)
+      params = [(cpt, NoParameterName)]
+  proc <- fmap repair $ function funName params i32 $ \[cur] -> do
+    l' <- forM li $ \i -> do
+      field <- gep cur [lit 0, lit 1, lit i]
+      load field 0
+    compile Res l' >>= ret
+
+  return (proc, free)
 
 thunk :: Lam Operand -> FunB Operand
 thunk l = do
-  let (h, free) = uniq l
-      nfields = length free
+  (proc, free) <- lift (func l)
   alloc <- view allocClosure
-  clos <- call alloc [(lit64 nfields, [])]
+  clos <- call alloc [(lit64 (length free), [])]
 
+  enter <- gep clos [lit 0, lit 0]
+  store enter 0 proc
   forM (zip free [0..]) $ \(fr, i) -> do
     field <- gep clos [lit 0, lit 1, lit i]
     store field 0 fr
 
-  proc <- func "thunk" 1 $ \[cur] ->
-    fmap h . forM [0..nfields - 1] $ \i -> do
-      field <- gep cur [lit 0, lit 1, lit i]
-      load field 0
-  enter <- gep clos [lit 0, lit 0]
-  store enter 0 proc
-
   return clos
 
-force :: Lam Operand -> FunB Operand
-force l = do
-  let (h, free) = uniq l
-      nfields = length free
-  proc <- func "force" nfields $ pure . h
-  call proc (zip free (repeat []))
-
-compile :: Lam Operand -> FunB ()
-compile l = case l of
+compile :: Pos -> Lam Operand -> FunB Operand
+compile p l = case l of
   Var clos -> do
     procP <- gep clos [lit 0, lit 0]
     proc <- load procP 0
-    -- builder API doesn't support indicating tail calls :(
-    let instr = Call {
-          tailCallKind = Just MustTail,
+    let t = case p of Res -> Just MustTail; Scrut -> Nothing
+        -- builder API doesn't support indicating tail calls :(
+        instr = Call {
+          tailCallKind = t,
           callingConvention = CC.C,
           returnAttributes = [],
           AST.function = Right proc,
           arguments = [(clos, [])],
           functionAttributes = [],
           metadata = []}
-    x <- emitInstr i32 instr
-    ret x
+    emitInstr i32 instr
   Abs name b -> do
     pop <- view popArg
     arg <- call pop []
-    compile (instantiate1 (Var arg) b)
+    compile p (instantiate1 (Var arg) b)
   App f x -> do
     push <- view pushArg
     clos <- thunk x
     call push [(clos, [])]
-    compile f
-  Lit i -> ret (lit i)
+    compile p f
+  Lit i -> pure (lit i)
   Op op x y -> do
-    x' <- force x
-    y' <- force y
+    x' <- compile Scrut x
+    y' <- compile Scrut y
     let [t, f] = map (lit . hashCode . Name) ["True", "False"]
         sel b = select b t f
-    i <- case op of
+    case op of
       Add -> add x' y'
       Sub -> sub x' y'
       Eq  -> icmp IP.EQ x' y' >>= sel
       Leq -> icmp IP.SLE x' y' >>= sel
-    ret i
   Ctor name fs -> do
     push <- view pushArg
     forM (reverse fs) $ \f -> do
       clos <- thunk f
       call push [(clos, [])]
-    ret . lit . hashCode $ name
+    pure . lit . hashCode $ name
   Case x cs -> undefined
-
 
 compileMain :: Lam Operand -> ModuleBuilder ()
 compileMain l = do
   env <- preamble
-  let func = function "main" [] i32 (const (compile l))
-  void (evalStateT (runReaderT func env) 0)
+  (outer, _) <- evalStateT (runReaderT (func l) env) 0
+  let undef = ConstantOperand (K.Undef (_closPtrType env))
+  void . function "main" [] i32 . const $
+    call outer [(undef, [])] >>= ret
 
 serialize :: Module -> IO ByteString
 serialize mod = withContext $ \ctx ->
