4fa067577198535e9d7b9a99b17b3122be892141
benzrf <benzrf@benzrf.com>
2017-12-25 23:16:54 -0500

    this is REALLY bad. TODO: make it suck less

--- LLVM2.hs
+++ LLVM2.hs
@@ -16,7 +16,7 @@ import qualified LLVM.AST as AST
 import qualified LLVM.AST.CallingConvention as CC
 import qualified LLVM.AST.Constant as K
 import qualified LLVM.AST.IntegerPredicate as IP
-import LLVM.AST.Type (i32, ptr)
+import LLVM.AST.Type hiding (void)
 import LLVM.IRBuilder
 import qualified LLVM.Module as L
 import LLVM.Context
@@ -25,78 +25,109 @@ import Preamble
 -- looks like the place we'll need fresh names is in the monad that
 -- DOESN'T supply them...
 type ModB = ReaderT Env (StateT Int ModuleBuilder)
+
+data Args =
+  Args {
+    stack :: Operand,
+    argCount :: Operand,
+    argChange :: (Int, Int)
+  } deriving (Show)
 -- the state is the operand containing the current top of the stack
-type FunB = StateT Operand (IRBuilderT ModB)
+type FunB = IRBuilderT ModB
 
-push :: Operand -> FunB ()
-push x = do
-  stk <- get
+resFst, resSnd :: MonadIRBuilder m => Operand -> m Operand
+resFst res = emitInstr i32 (ExtractValue res [0] [])
+resSnd res = emitInstr closP (ExtractValue res [1] [])
+
+push :: Args -> Operand -> FunB Args
+push (Args stk argc (minChange, netChange)) x = do
   stk' <- gep stk [lit 1] `named` "stack"
-  put stk'
   store stk' 0 x
+  return (Args stk' argc (minChange, netChange + 1))
 
-pop :: FunB Operand
-pop = do
-  cpt <- view closPtrType
-  stk <- get
+pop :: Args -> FunB (Operand, Args)
+pop (Args stk argc (minChange, netChange)) = do
   x <- load stk 0
-  store stk 0 (ConstantOperand (K.Undef cpt))
+  store stk 0 (undef closP)
   stk' <- gep stk [lit (-1)] `named` "stack"
-  put stk'
-  return x
+  let net' = netChange - 1
+  return (x, Args stk' argc (min minChange net', net'))
 
 data Pos r where
   Res :: Pos ()
   Scrut :: Pos Operand
 deriving instance Show (Pos r)
 
-adapt :: Pos r -> FunB Operand -> FunB r
-adapt Res o = o `named` "res" >>= ret
-adapt Scrut o = o
+result :: Pos r -> FunB Operand -> FunB r
+result Res o = o `named` "res" >>= ret
+result Scrut o = o
 
-func :: Lam Operand -> ModB (Operand, [Operand])
-func l = do
+mkClosure :: Operand -> [Operand] -> FunB Operand
+mkClosure proc fields = do
+  alloc <- view allocClosure
+  clos <- call alloc [(lit64 (length fields), []), (proc, [])]
+
+  when (not (null fields)) $ do
+    fsp <- gep clos [lit 0, lit 1] `named` "fieldsp_new"
+    fs <- load fsp 0 `named` "fields_new"
+    forM_ (zip fields [0..]) $ \(fr, i) -> do
+      field <- gep fs [lit i] `named` "fieldp_new"
+      store field 0 fr
+
+  return clos
+
+mkProc :: ((Operand, Operand, Operand) -> FunB ()) -> ModB Operand
+mkProc body = do
+  freshNum <- id <<+= 1
+  let funName = mkName ("proc." ++ show freshNum)
+      params = [(closP, ParameterName "cur_clos"),
+                (ptr closP, ParameterName "stack"),
+                (i32, ParameterName "arg_count")]
+      body' [cur, stk, argc] = body (cur, stk, argc) `named` "var"
+  fmap repair . function funName params resTy $ body'
+
+closureProc :: Lam Operand -> ModB (Operand, [Operand])
+closureProc l = do
   let (li, free) = runState (traverse (state . replace) l) []
       replace v seen = case findIndex (==v) seen of
         Nothing -> (length seen, seen ++ [v])
         Just i  -> (i, seen)
 
-  cpt <- view closPtrType
-  freshNum <- id <<+= 1
-  let funName = mkName ("proc." ++ show freshNum)
-      params = [(cpt, ParameterName "cur_clos"),
-                (ptr cpt, ParameterName "stack")]
-  proc <- fmap repair $ function funName params i32 $ \[cur, stk] -> do
-    fields <- forM [0..length free - 1] $ \i -> do
-      field <- gep cur [lit 0, lit 1, lit i] `named` "closedp"
-      load field 0 `named` "closed"
+  proc <- mkProc $ \(cur, stk, argc) -> do
+    fields <- case free of
+      [] -> return []
+      _ -> do
+        fsp <- gep cur [lit 0, lit 1] `named` "fieldsp_cur"
+        fs <- load fsp 0 `named` "fields_cur"
+        forM [0..length free - 1] $ \i -> do
+          field <- gep fs [lit i] `named` "closedp"
+          load field 0 `named` "closed"
     let l' = (fields !!) <$> li
-    evalStateT (compile Res l') stk
+    compile Res (Args stk argc (0, 0)) l'
 
   return (proc, free)
 
 thunk :: Lam Operand -> FunB Operand
 thunk (Var clos) = return clos
+thunk (Ctor name fs) = do
+  proc <- lift . mkProc $ \(cur, stk, argc) -> do
+    let members = [K.Int 32 (toInteger (hashCode name)), K.Undef closP]
+    s <- struct (Just resTyName) False members
+    emitInstr resTy (InsertValue s cur [1] []) >>= ret
+  fields <- forM fs $ \f -> thunk f `named` "field_new"
+  mkClosure proc fields
 thunk l = do
-  (proc, free) <- lift . lift . func $ l
-  alloc <- view allocClosure
-  clos <- call alloc [(lit64 (length free), []), (proc, [])]
+  (proc, free) <- lift (closureProc l)
+  mkClosure proc free
 
-  forM_ (zip free [0..]) $ \(fr, i) -> do
-    field <- gep clos [lit 0, lit 1, lit i] `named` "fieldp"
-    store field 0 fr
+scrutinize :: Args -> Lam Operand -> FunB Operand
+scrutinize (Args stk _ _) = compile Scrut (Args stk (lit 0) (0, 0))
 
-  return clos
-
-nest :: FunB a -> FunB (a, Operand)
-nest a = get >>= lift . runStateT a
-
-compile :: Pos r -> Lam Operand -> FunB r
-compile p l = case l of
+compile :: Pos r -> Args -> Lam Operand -> FunB r
+compile p args@(Args stk argc _) l = case l of
   Var clos -> do
     enter <- gep clos [lit 0, lit 0] `named` "enter"
     proc <- load enter 0 `named` "proc"
-    stk <- get
     let t = case p of Res -> Just MustTail; Scrut -> Nothing
         -- builder API doesn't support indicating tail calls :(
         instr = Call {
@@ -104,51 +135,73 @@ compile p l = case l of
           callingConvention = CC.C,
           returnAttributes = [],
           AST.function = Right proc,
-          arguments = [(clos, []), (stk, [])],
+          arguments = [(clos, []), (stk, []), (argc, [])],
           functionAttributes = [],
           metadata = []}
-    adapt p $ emitInstr i32 instr
+    result p $ emitInstr resTy instr
   Abs (Name s) b -> do
-    arg <- pop `named` fromString s
-    compile p (instantiate1 (Var arg) b)
+    (arg, args') <- pop args `named` fromString s
+    compile p args' (instantiate1 (Var arg) b)
   App f x -> do
     clos <- thunk x `named` "arg"
-    push clos
-    compile p f
-  Lit i -> adapt p (return (lit i))
+    args' <- push args clos
+    compile p args' f
+  Lit i -> do
+    s <- struct (Just resTyName) False [K.Undef i32, K.Undef closP]
+    sval <- emitInstr resTy (InsertValue s (lit i) [0] [])
+    case p of
+      Res -> do
+        pval <- inttoptr (lit i) closP
+        proc <- view iproc
+        clos <- mkClosure proc [pval]
+        emitInstr resTy (InsertValue sval clos [1] []) >>= ret
+      Scrut -> return sval
   Op op x y -> do
-    (x', _) <- nest (compile Scrut x) `named` "lhs"
-    (y', _) <- nest (compile Scrut y) `named` "rhs"
+    x' <- scrutinize args x `named` "lhs" >>= resFst
+    y' <- scrutinize args y `named` "rhs" >>= resFst
     let [t, f] = map (lit . hashCode . Name) ["True", "False"]
         sel b = select b t f
-    adapt p $ case op of
+    val <- flip named "val" $ case op of
       Add -> add x' y'
       Sub -> sub x' y'
       Eq  -> icmp IP.EQ x' y' >>= sel
       Leq -> icmp IP.SLE x' y' >>= sel
+    s <- struct (Just resTyName) False [K.Undef i32, K.Undef closP]
+    sval <- emitInstr resTy (InsertValue s val [0] [])
+    case p of
+      Res -> do
+        pval <- inttoptr val closP
+        proc <- view iproc
+        clos <- mkClosure proc [pval]
+        emitInstr resTy (InsertValue sval clos [1] []) >>= ret
+      Scrut -> return sval
   Ctor name fs -> do
-    forM_ (reverse fs) $ \f -> do
-      clos <- thunk f `named` "field"
-      push clos
-    adapt p . return . lit . hashCode $ name
+    clos <- thunk l `named` "ctor"
+    let members = [K.Int 32 (toInteger (hashCode name)), K.Undef closP]
+    s <- struct (Just resTyName) False members
+    result p $ emitInstr resTy (InsertValue s clos [1] [])
   Case x cs -> do
-    (x', _) <- nest (compile Scrut x) `named` "scrutinee"
+    x' <- scrutinize args x `named` "scrutinee"
+    hash <- resFst x' `named` "hash"
+    clos <- resSnd x' `named` "clos"
+    fsp <- gep clos [lit 0, lit 1] `named` "fieldsp_res"
     defaultLabel <- freshName "default"
     resLabel <- freshName "res"
     branchLabels <- forM cs $ \c -> (,) c <$> freshName "branch"
     let dest (Clause name _ _, label) =
           (K.Int 32 (toInteger (hashCode name)), label)
-    switch x' defaultLabel . map dest $ branchLabels
-    phis <- forM branchLabels $ nest . \(Clause name names b, label) -> do
+    switch hash defaultLabel . map dest $ branchLabels
+    phis <- forM branchLabels $ \(Clause name names b, label) -> do
       emitBlockStart label
-      stk <- get
-      gep stk [lit (length names)] `named` "stack" >>= put
-      fields <- replicateM (length names) pop `named` "field"
-      r <- compile p (instantiateVars fields b)
+      fields <- forM [0..length names - 1] $ \i -> do
+        fs <- load fsp 0 `named` "fields_res"
+        fieldp <- gep fs [lit i] `named` "fieldp_res"
+        load fieldp 0 `named` "field_res"
+      r <- compile p args (instantiateVars fields b)
       () <- case p of Res -> return (); Scrut -> br resLabel
       -- this is kinda hacky...
-      let l IRBuilderState{builderBlock=
-        Just PartialBlock{partialBlockName=name}} = name
+      let l IRBuilderState {builderBlock =
+        Just PartialBlock {partialBlockName = name}} = name
       curLabel <- liftIRState (gets l)
       return (r, curLabel)
     emitBlockStart defaultLabel
@@ -157,21 +210,19 @@ compile p l = case l of
       Res -> return ()
       Scrut -> do
         emitBlockStart resLabel
-        let stkPhis = map (\((_, l), s) -> (s, l)) phis
-            resPhis = map fst phis
-        phi stkPhis `named` "stack" >>= put
-        phi resPhis
+        phi phis
 
 compileMain :: Lam Operand -> ModuleBuilder ()
 compileMain l = do
   env <- preamble
-  (outer, _) <- evalStateT (runReaderT (func l) env) 0
-  let undef = ConstantOperand (K.Undef (_closPtrType env))
-      calloc = _calloc env
+  let body (cur, stk, argc) = compile Res (Args stk argc (0, 0)) l
+  outer <- evalStateT (runReaderT (mkProc body) env) 0
+  let calloc = _calloc env
   void . function "main" [] i32 . const $ do
     stkMem <- call calloc [(lit64 3000, []), (lit64 8, [])] `named` "stack_mem"
-    stk <- bitcast stkMem (ptr (_closPtrType env)) `named` "stack"
-    call outer [(undef, []), (stk, [])] `named` "res" >>= ret
+    stk <- bitcast stkMem (ptr closP) `named` "stack"
+    res <- call outer [(undef closP, []), (stk, []), (lit 1, [])] `named` "res"
+    resFst res >>= ret
 
 serialize :: Module -> IO ByteString
 serialize mod = withContext $ \ctx ->
--- Preamble.hs
+++ Preamble.hs
@@ -12,10 +12,13 @@ lit, lit64 :: Int -> Operand
 lit = runIdentity . int32 . toInteger
 lit64 = runIdentity . int64 . toInteger
 
+undef :: Type -> Operand
+undef = ConstantOperand . K.Undef
+
 data Env =
   Env {
-    _closPtrType :: Type,
     _allocClosure :: Operand,
+    _iproc :: Operand,
     _calloc :: Operand} deriving (Show)
 makeLenses ''Env
 
@@ -25,36 +28,55 @@ repair (ConstantOperand (K.GlobalReference ty name)) =
   ConstantOperand (K.GlobalReference (ptr ty) name)
 repair _ = error "misuse of repair"
 
+closTyName, resTyName :: Name
+closP, resTy, resDef, funTy, closDef :: Type
+closTyName = "closure"
+resTyName = "result"
+closP = ptr (NamedTypeReference closTyName)
+resTy = NamedTypeReference resTyName
+resDef = StructureType {isPacked = False, elementTypes = [i32, closP]}
+funTy = ptr
+  FunctionType {
+    resultType = resTy,
+    argumentTypes = [closP, ptr closP, i32],
+    isVarArg = False}
+closDef = StructureType {
+  isPacked = False,
+  elementTypes = [
+    funTy,
+    ptr closP]}
+
 preamble :: ModuleBuilder Env
 preamble = do
-  let ctn = "closure"
-      closP = ptr (NamedTypeReference ctn)
-      fty = ptr (FunctionType {
-              resultType = i32,
-              argumentTypes = [closP, ptr closP],
-              isVarArg = False})
-      cty  = StructureType {
-        isPacked = False,
-        elementTypes = [
-          fty,
-          ArrayType {
-            nArrayElements = 0,
-            elementType = closP}]}
-  typedef ctn (Just cty)
+  typedef closTyName (Just closDef)
+  typedef resTyName (Just resDef)
   calloc <- repair <$> extern "calloc" [i64, i64] (ptr i8)
   allocClosure <- fmap repair . function "alloc_closure"
-    [(i64, ParameterName "field_count"), (fty, ParameterName "proc")]
+    [(i64, ParameterName "field_count"), (funTy, ParameterName "proc")]
     closP $ \[fc, proc] -> do
-    fsize <- mul fc (lit64 8) `named` "fsize"
-    size <- add fsize (lit64 8) `named` "size"
-    closMem <- call calloc [(lit64 1, []), (size, [])] `named` "mem"
+    fsMem <- call calloc [(fc, []), (lit64 8, [])] `named` "fields_mem"
+    fs <- bitcast fsMem (ptr closP) `named` "fields"
+    closMem <- call calloc [(lit64 1, []), (lit64 16, [])] `named` "clos_mem"
     clos <- bitcast closMem closP `named` "clos"
     enter <- gep clos [lit 0, lit 0] `named` "enter"
     store enter 0 proc
+    fsp <- gep clos [lit 0, lit 1] `named` "fieldsp"
+    store fsp 0 fs
     ret clos
+  let params = [(closP, ParameterName "cur_clos"),
+                (ptr closP, ParameterName "stack"),
+                (i32, ParameterName "arg_count")]
+  iproc <- fmap repair $ function "iproc"
+    params resTy $ \[cur, stk, argc] -> do
+    fsp <- gep cur [lit 0, lit 1] `named` "fieldsp"
+    fs <- load fsp 0
+    val <- ptrtoint fs i32
+    s <- struct (Just resTyName) False [K.Undef i32, K.Undef closP]
+    sval <- emitInstr resTy (InsertValue s val [0] [])
+    emitInstr resTy (InsertValue sval cur [1] []) >>= ret
   return Env {
-    _closPtrType = closP,
     _allocClosure = allocClosure,
+    _iproc = iproc,
     _calloc = calloc
   }
 
